9f1f0214ac119433e700e0bc21c43aa192a5b4fd
Minyu Zhang <minyuzhang@hotmail.com>
2025-05-08 14:57:28 -0500

    day4 arrays

--- vanilla_homework/day4_js_basic1/arrays.js
+++ vanilla_homework/day4_js_basic1/arrays.js
@@ -6,6 +6,10 @@ export function checkIfStringIsNumber(str) {
   //   returns a boolean indicating if the str is a number
   //   Example:
   //   '1' -> true, "a" -> false, "1a" -> false
+  if (typeof str === 'number'){
+    return true
+}
+return false
 }
 
 export function findAvgOfNums(arr) {
@@ -13,6 +17,13 @@ export function findAvgOfNums(arr) {
   //   argument and returns the average of all the numbers.
   //   Example: const arr = [1, '2', 3, '4', 5];
   //   Expected output: 3
+  let res = 0;
+  for (let number of arr){
+      res +=Number(number)
+  }
+
+  return res/arr.length;
+
 }
 
 export function findAverageAge(people) {
@@ -20,6 +31,11 @@ export function findAverageAge(people) {
   //   returns the average age of all the people.
   //   Do not use prototype methods
   //   Example: const people = [{name: 'John', age: 21}, {name: 'Alice', age: 25}];
+  let res = 0;
+  for (let person of people){
+      res+=person.age
+  }
+  return res/people.length
 }
 
 export function findAvgAgeByJob(people, job) {
@@ -28,6 +44,15 @@ export function findAvgAgeByJob(people, job) {
   //   Do not use prototype methods
   //   Example: const people = [{name: 'John', age: 21, job: 'teacher'}, {name: 'Alice', age: 25, job: 'teacher'}];
   //   Expected output: 23 (teacher)
+  let res = 0;
+  let count = 0;
+  for (let person of people){
+      if (person.job === job){
+          res += person.age;
+          count++
+      }
+  }
+  return res/count;
 }
 
 export function findMaxNum(arr) {
@@ -35,6 +60,14 @@ export function findMaxNum(arr) {
   //   returns the maximum number in that array.
   //   Do not use Math.max
   //   Example: const arr = [1, 2, 3, 4, 5];
+  let max = -Infinity;
+  for (let number of arr){
+      if (number > max){
+          max = number
+      }
+  }
+
+  return max;
 }
 
 export function findLongestWord(str) {
@@ -42,6 +75,17 @@ export function findLongestWord(str) {
   //   returns the longest word in that string.
   //   Hint: You can use String.prototype.split
   //   Example: const str = 'I love JavaScript';
+
+  let strArr = str.split(' ')
+  let longerst = 0;
+  let res;
+  for (let i = 0; i < strArr.length; i++){
+      if (strArr[i].length > longerst){
+          longerst = strArr[i].length;
+          res = strArr[i]
+      }
+  }
+  return res;
 }
 
 export function findSumOfEvenNums(arr) {
@@ -49,6 +93,13 @@ export function findSumOfEvenNums(arr) {
   //   returns an array of only the even numbers.
   //   Example: const arr = [1, 2, 3, 4, 5];
   //   Expected output: [2, 4]
+  let res = [];
+  for (let number of arr){
+      if (number % 2 === 0){
+          res.push(number)
+      }
+  }
+  return res;
 }
 
 // reference types
@@ -58,6 +109,17 @@ export function bubbleSortArr1(num) {
   // Do not use Array.prototype.sort
   // Example: const num = [5, 3, 8, 2, 1];
   // Expected output: [1, 2, 3, 5, 8]
+  let res = [...num];
+  let n = num.length;
+  for (var i = 0; i < n; i++) { 
+      for (var j = 0; j < n - i - 1; j++){
+          if (res[j]>res[j+1]){
+              [res[j], res[j+1]] = [res[j+1], res[j]]
+          }
+      }
+
+  }
+    return res;
 }
 
 export function bubbleSortArr2(num) {
@@ -70,6 +132,14 @@ export function removeTypes(arr, typeToRemove) {
   // Example: const arr = [1, '2', 3, '4', 5];
   // removeTypes(arr, 'string');
   // Expected output: [1, 3, 5]
+  let res = []
+  for (let num of arr){
+    if (typeof num !== typeToRemove){
+      res.push(num)
+
+    }
+  }
+  return res;
 }
 
 // reinvent the wheel: prototype methods
@@ -79,6 +149,17 @@ export function changeNumsByAmount(nums, amount, operator) {
   // Example: const nums = [1, 2, 3, 4, 5];
   // changeNumsByAmount(nums, 2, '+');
   // Expected output: [3, 4, 5, 6, 7]
+  if (operator === '+'){
+    return nums.map(num => num+amount)
+}else if (operator === '-'){
+    return nums.map(num => num-amount)
+}else if (operator === '*'){
+    return nums.map(num => num*amount)
+}else if (operator === '/'){
+    return nums.map(num => num/amount)
+} else {
+    console.log('unsupported operator')
+}
 }
 
 export function removeNumFromArr(nums, num) {
@@ -87,4 +168,11 @@ export function removeNumFromArr(nums, num) {
   // Example: const nums = [1, 2, 3, 3, 3, 4, 5];
   // removeNumFromArr(nums, 3);
   // Expected output: [1, 2, 4, 5]
+  let res = [];
+  for (let number of nums){
+    if (number !== num){
+      res.push(number)
+    }
+  }
+  return res;
 }
--- vanilla_homework/day4_js_basic1/scopes.js
+++ vanilla_homework/day4_js_basic1/scopes.js
@@ -6,16 +6,16 @@ function accessGlobal() {
   return globalVar;
 }
 
-// console.log(accessGlobal()); // Expected output?
-// console.log(globalVar);      // Expected output?
+//  console.log(accessGlobal()); // Expected output? "I am global"
+//  console.log(globalVar);      // Expected output?"I am global"
 
 // Local scope
 function localScope() {
   var localVar = "I am local to this function";
 }
 
-// localScope();
-// console.log(localVar); // What happens here and why?
+localScope();
+//console.log(localVar); // What happens here and why? //referenceError, LocalVar is not defined
     
 // Block scope
 if (true) {
@@ -23,20 +23,20 @@ if (true) {
   const blockConst = "So do I!";
 }
 
-// console.log(blockVar); // What is the result and why?
-// console.log(blockConst); // What is the result and why?
+// console.log(blockVar); // What is the result and why? ReferenceError: blockVar is not defined becasue let and coust are block scope
+// console.log(blockConst); // What is the result and why? blockConst is not defined
 
 // Function scope vs block scope
 function loopScope() {
   for (var i = 0; i < 3; i++) {
-    // console.log(i); // Outputs during loop?
+    // console.log(i); // Outputs during loop? 0,1,2
   }
-  //   console.log("After loop with var: " + i); // What happens here?
+   // console.log("After loop with var: " + i); // What happens here?After loop with var: 3 because var is function scope
 
   for (let j = 0; j < 3; j++) {
-    console.log(j); // Outputs during loop?
+ //   console.log(j); // Outputs during loop?
   }
-  //   console.log("After loop with let: " + j); // What happens here?
+    console.log("After loop with let: " + j); // What happens here?  j is not defined becasue let is block scope
 }
 
 loopScope();
