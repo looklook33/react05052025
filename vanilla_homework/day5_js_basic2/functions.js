c493e8eef7083419b252ef54b5fff0f657e87973
Minyu Zhang <minyuzhang@hotmail.com>
2025-05-09 19:36:42 -0500

    finished day5 coding

--- vanilla_homework/day5_js_basic2/functions.js
+++ vanilla_homework/day5_js_basic2/functions.js
@@ -1,14 +1,34 @@
-export function sum() {
+export function sum(num1=0,num2=0) {
   // write a function that takes two numbers as arguments and returns their sum
   // argument default values are 0
   // if wrong data type is passed, throw an error
   // Define your function here
-  return () => {};
-}
+      if ( typeof num1 !== 'number' || typeof num2 !== 'number'){
+      throw new Error();
+    } 
+    return num1+num2
 
-export function sumOfAll() {
+}
+// console.log(sum(1, 2))
+
+
+export function sumOfAll(...nums) {
   // write a function that takes any number of arguments and returns their sum
   // if wrong data type is passed, throw an error
   // Define your function here
-  return () => {};
+  // return (...nums) => {
+    let res=0
+    for (const num of nums){
+      if (typeof num !== 'number'){
+        throw new Error();
+      } else if( num === undefined)
+        {
+          num = 0;
+          res +=num
+      } else{
+         res +=num
+      }
+    }
+    return res;
+  // };
 }
--- vanilla_homework/day5_js_basic2/prototypes.js
+++ vanilla_homework/day5_js_basic2/prototypes.js
@@ -6,6 +6,12 @@ export function myMap(arr, cb) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myMap(arr, num => num * 2);
   // Expected output: [2, 4, 6, 8, 10]
+  const res = [];
+  for (let i = 0; i < arr.length; i++){
+    res.push(cb(arr[i]))
+  }
+  return res;
+
 }
 
 export function myFilter(arr, cb) {
@@ -14,6 +20,13 @@ export function myFilter(arr, cb) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myFilter(arr, num => num % 2 === 0);
   // Expected output: [2, 4]
+  const res = [];
+  for (let i = 0; i<arr.length; i++){
+    if (cb(arr[i])){
+      res.push(arr[i])
+    }
+  }
+  return res;
 }
 
 export function myConcat(arr1, arr2) {
@@ -23,6 +36,7 @@ export function myConcat(arr1, arr2) {
   // const arr2 = [4, 5, 6];
   // myConcat(arr1, arr2);
   // Expected output: [1, 2, 3, 4, 5, 6]
+  return [...arr1,...arr2]
 }
 
 export function myFind(arr, cb) {
@@ -31,6 +45,11 @@ export function myFind(arr, cb) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myFind(arr, num => num % 2 === 0);
   // Expected output: 2
+  for (let i = 0; i < arr.length; i++){
+    if (cb(arr[i])){
+      return arr[i]
+    }
+  }
 }
 
 export function myEvery(arr, cb) {
@@ -39,6 +58,12 @@ export function myEvery(arr, cb) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myEvery(arr, num => num > 0);
   // Expected output: true
+  for (let i = 0; i < arr.length; i++){
+    if (!cb(arr[i])){
+      return false
+    }
+  }
+  return true
 }
 
 export function mySome(arr, cb) {
@@ -47,6 +72,12 @@ export function mySome(arr, cb) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // mySome(arr, num => num % 2 === 0);
   // Expected output: true
+   for(let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])){
+      return true
+    }
+  }
+  return false
 }
 
 export function myIncludes(arr, val) {
@@ -55,6 +86,12 @@ export function myIncludes(arr, val) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myIncludes(arr, 3);
   // Expected output: true
+     for(let i = 0; i < arr.length; i++) {
+    if (arr[i]===val){
+      return true
+    }
+  }
+  return false
 }
 
 export function myJoin(arr, separator) {
@@ -63,6 +100,7 @@ export function myJoin(arr, separator) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myJoin(arr, '-');
   // Expected output: '1-2-3-4-5'
+  return arr.join(separator);
 }
 
 export function myPush(arr, val) {
@@ -71,6 +109,7 @@ export function myPush(arr, val) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myPush(arr, 6);
   // Expected output: [1, 2, 3, 4, 5, 6]
+  return arr.push(val)
 }
 
 export function myReverse1(arr) {
@@ -79,10 +118,16 @@ export function myReverse1(arr) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myReverse(arr);
   // Expected output: [5, 4, 3, 2, 1]
+  let res = [];
+  for (let i = arr.length-1; i >=0; i--){
+    res.push(arr[i])
+  }
+  return res;
 }
 
 export function myReverse2(arr) {
   // Same as above but this time returns the original array reference reversed.
+  return arr.reverse();
 }
 
 // Challenges
@@ -92,6 +137,12 @@ export  function myReduce(arr, cb, initial) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // myReduce(arr, (acc, num) => acc + num, 0);
   // Expected output: 15
+    let res = initial;
+  for (let i = 0; i < arr.length; i++){
+    res = cb(res, arr[i])
+    
+  }
+  return res;
 }
 
 export function mySort(arr, cb) {
@@ -100,12 +151,38 @@ export function mySort(arr, cb) {
   // Example: const arr = [1, 2, 3, 4, 5];
   // mySort(arr, (a, b) => a - b);
   // Expected output: [1, 2, 3, 4, 5]
+  let newArr =[...arr];
+  //console.log(newArr)
+  for (let i = 0; i < newArr.length-1; i++){
+    for (let j = 1+i; j < newArr.length; j++){
+      if (cb(newArr[i], newArr[j]) >0){
+        let temp = newArr[i];
+        newArr[i]= newArr[j];
+        newArr[j]=temp
+      }
+    }
+  }
+  return newArr;
+
 }
 
-export function mySlice(arr, start, end) {
+export function mySlice(arr, start, end=arr.length) {
   // Write a function that takes an array, a start index and an end index as arguments
   // and returns a new array with the elements sliced from the start to the end.
   // Example: const arr = [1, 2, 3, 4, 5];
   // mySlice(arr, 1, 3);
   // Expected output: [2, 3]
+  
+  let res = [];
+
+  if (start < 0){
+    for (let i = arr.length+start; i < arr.length; i++){
+      res.push(arr[i])
+    }
+  } else {
+      for (let i = start; i < end; i++){
+    res.push(arr[i])
+  }
+  }
+  return res;
 }
